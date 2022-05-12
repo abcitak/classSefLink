53814ca2df5bb8eac9164eb9bf4a83ba34117b33
Ahmet ÇITAK <ahmetb.citak@gmail.com>
2022-05-13 00:25:55 +0300

    Simple Seflink with Class
    Class kullanarak basit bir seflink oluşturdum. Kullanımı oldukça kolay ve yeni başlayanlar için oldukça kullanışlı olacaktır. Projeye gelecek olursak;
    Önce SefLink adında bir class oluşturdum. Daha sonra $strSef adında bir değişken oluşturdum ki bu değişken bizim ana sistemimizin bir parçasıdır. Bir tane fonksiyon tanımladım ve önce büyük harfler varsa küçültsün(burada UTF-8 olmasının sebebi mesela büyük "İ" gibi karakterlerde de işe yaraması için), türkçe karakterleri başka harflerle değiştirmek ve harflerin veya sayıların arasında kalan boşluklar için tire (-) işareti koydum. Son olarak tüm bu değerleri geri döndürdüm. Tabiki bu basit bir seflink yapısıdır. Daha detaylı ve farklı diller için internette araştırabilirsiniz.

--- classSefLink.php
+++ classSefLink.php
@@ -6,14 +6,19 @@ class SefLink
 
     public function Generetor($strSef)
     {
+        //türkçe karakterler için
         $strSef = mb_strtolower($strSef,'UTF-8');
+        // türkçe karakterleri farklı harflerle değiştirmek için
         $strSef = str_replace(
             ['ı','ş','ç','ü','ö','ğ'],
             ['i','s','c','u','o','g'],
             $strSef
         );
+        // harf ve sayıların arasında kalan boşlukları tire ile değiştirmek
         $strSef = preg_replace('/[^a-z0-9]/','-',$strSef);
+        // özel işaretleri de tire ile değiştirmek için. mesela; ? * + gibi.
         $strSef = preg_replace('/-+/','-',$strSef);
+        // sağda ve solda kalan boşluk veya özel işaret nedeni ile tireye dönüşen karakterleri silmek için
         $strSef = trim($strSef,'-');
         
         return $strSef;
@@ -21,7 +26,7 @@ class SefLink
 }
 
 $sefLink = new SefLink;
-$str = 'herşey çok güzel olsada oğzaman güzeldır ya da güzel değildir şanki';
+$str = 'herşey çok güzel olsada oğzaman güzeldır ya da güzel değildir şanki ?';
 echo $sefLink->Generetor($str);
 
 
